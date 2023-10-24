eea13b087b6f3dea4d6d657bff47f0184a1a41da
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-23 21:49:37 -0300

    Update settings.py

--- Mecha/settings.py
+++ Mecha/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tasks/static')]
-
+print(STATICFILES_DIRS)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "tasks" / "static" / "media"
 
