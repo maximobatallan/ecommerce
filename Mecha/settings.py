676db27fe74b7aeb224faa6ad4b8f983abd5dea4
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-23 22:45:30 -0300

    Update settings.py

--- Mecha/settings.py
+++ Mecha/settings.py
@@ -133,8 +133,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "tasks" / "static" / "media"
 
 LOGIN_URL = '/signin/'
 
