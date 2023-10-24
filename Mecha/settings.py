8cfda96675d1a94a3209425d80f903f806a64f2d
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-23 23:02:06 -0300

    Update settings.py

--- Mecha/settings.py
+++ Mecha/settings.py
@@ -130,11 +130,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/tasks/static/',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "tasks/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 LOGIN_URL = '/signin/'
